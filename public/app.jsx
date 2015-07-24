6ffff7cba7801451bf846f7180b3ac8a77619a29
jincod <vadim.abdrashitov@gmail.com>
2015-07-24 22:50:45 +0500

    hotkeys and different track sources

--- public/app.jsx
+++ public/app.jsx
@@ -17,34 +17,55 @@ class App extends React.Component {
 
 		this.audio = audiojs.createAll({
 			trackEnded: function() {
-				self.playNextTrack();
+				if(self.state.currentTrack === (self.state.tracks.length - 1)) {
+					self.setState({currentTrack: 0}, () => {
+						self._playCurrentTrack();
+					});
+				} else {
+					self.playNextTrack();
+				}
 			}
 		})[0];
 		superagent
 			.get('/api/track')
-			.withCredentials()
+			.query({query: window.location.hash.replace('#/', '')})
 			.end(function(err, res) {
 				let currentTrack = localStorage.getItem("currentTrack") && parseInt(localStorage.getItem("currentTrack"), 10) || 0;
 				self.setState({tracks: res.body, currentTrack: currentTrack}, function() {
-					self._playCurrentTrack();
+					this.audio.load(this.state.tracks[this.state.currentTrack].url);
 				});
 			});
+		$(document).keydown((e) => {
+			let unicode = e.charCode ? e.charCode : e.keyCode;
+			// right arrow
+			if (unicode == 39) {
+				this.playNextTrack();
+				// back arrow
+			} else if (unicode == 37) {
+				this.playPrevTrack();
+				// spacebar
+			} else if (unicode == 32) {
+				this.audio.playPause();
+				e.preventDefault();
+			}
+		})
 	}
 	render() {
 		return (
-			<div>
-				<div>
-					<audio />
-				</div>
-				<div>
-					<a onClick={this.playNextTrack} className="btn btn-default">Next</a>
+			<div style={{width: 'inherit'}}>
+				<div className="wrapper">
+					<div className>
+						<audio/>
+					</div>
 				</div>
 				<ul className="list-group track-list">
 				{
 					this.state.tracks.map((track, index) => {
 						let currentClass = index === this.state.currentTrack ? "list-group-item active" : "list-group-item";
 						return (
-							<li key={index} onClick={this.playThisTrack.bind(this, index)} className={currentClass}>{index+1}. {track.artist} - {track.title}</li>
+							<li key={index} onClick={this.playThisTrack.bind(this, index)} className={currentClass}>
+								{index+1}. <span dangerouslySetInnerHTML={{__html: track.artist}}></span> - <span dangerouslySetInnerHTML={{__html: track.title}}></span>
+							</li>
 						)
 					})
 				}
@@ -62,7 +83,12 @@ class App extends React.Component {
 		this.audio.load(this.state.tracks[this.state.currentTrack].url);
 		this.audio.play();
 	}
-
+	playPrevTrack() {
+		var self = this;
+		this.setState({currentTrack: this.state.currentTrack - 1}, () => {
+			self._playCurrentTrack();
+		});
+	}
 	playNextTrack() {
 		var self = this;
 		this.setState({currentTrack: this.state.currentTrack + 1}, () => {
--- server/app.js
+++ server/app.js
@@ -1,15 +1,14 @@
 import express from 'express'
 import request from 'superagent'
 
-let app = express();
+const accessToken = process.env.ACCESS_TOKEN;
 
+let app = express();
 let apiRouter = express.Router();
 
-const userId = '-20833574';
-const accessToken = process.env.ACCESS_TOKEN;
-const url = 'https://api.vk.com/method/audio.get?access_token=' + accessToken + '&count=100&owner_id=' + userId;
-
-apiRouter.get('/track', (req, res) => {
+let getAudioTracks = (id, callback) => {
+	let url = 'https://api.vk.com/method/audio.get?access_token='
+		+ accessToken + '&owner_id=' + id;
 	request
 		.get(url)
 		.end((err, response) => {
@@ -17,8 +16,83 @@ apiRouter.get('/track', (req, res) => {
 				return res.sendStatus(400, err);
 			}
 			response.body.response.splice(0, 1);
-			res.send(response.body.response);
+			callback(response.body.response);
 		});
+}
+
+let getWallTracks = (id, callback) => {
+	let url = 'https://api.vk.com/method/wall.get?count=300&owner_id=' + id;
+	request
+		.get(url)
+		.end((err, response) => {
+			if(err) {
+				return res.sendStatus(400, err);
+			}
+
+			var result = [],
+				posts = response.body.response.slice(1);
+			for (var i = 0; i < posts.length; i++) {
+				var attachments = posts[i].attachments;
+				if(attachments) {
+					for (var j = 0; j < attachments.length; j++) {
+						var attachment = attachments[j];
+						if(attachment.audio) {
+							result.push(attachment.audio);
+						}
+					};
+				}
+			};
+			callback(result);
+		});
+}
+
+let getPostTracks = (id, callback) => {
+	let url = 'https://api.vk.com/method/wall.getById?count=300&posts=' + id;
+	request
+		.get(url)
+		.end((err, response) => {
+			if(err) {
+				return res.sendStatus(400, err);
+			}
+
+			var result = [],
+				posts = response.body.response;
+			for (var i = 0; i < posts.length; i++) {
+				var attachments = posts[i].attachments;
+				if(attachments) {
+					for (var j = 0; j < attachments.length; j++) {
+						var attachment = attachments[j];
+						if(attachment.audio) {
+							result.push(attachment.audio);
+						}
+					};
+				}
+			};
+			callback(result);
+		});
+}
+
+apiRouter.get('/track', (req, res) => {
+	let query = req.query.query.split(',')[0] || 'id49749181';
+
+	if(query.startsWith('id')) {
+		let id = query.replace('id', '');
+		getAudioTracks(id, (result) => {
+			res.send(result);
+		});
+	} else if(query.startsWith('wall') && query.indexOf('_') === -1) {
+		let id = query.replace('wall', '');
+		getWallTracks(id, (result) => {
+			res.send(result);
+		});
+	} else if(query.startsWith('wall') && query.indexOf('_') !== -1) {
+		let id = query.replace('wall', '');
+		getPostTracks(id, (result) => {
+			res.send(result);
+		});
+	} else {
+		res.send([]);
+	}
 });
 
 app.use(express.static("public"));
