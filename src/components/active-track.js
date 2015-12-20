cfa709f7310c77ceb6b63987a273ff7c7f30a078
jincod <vadim.abdrashitov@gmail.com>
2015-12-20 21:34:53 +0500

    rewrite active track on dumb component

--- src/components/active-track.js
+++ src/components/active-track.js
@@ -1,21 +1,21 @@
 import React from 'react'
 
-
-export class ActiveTrack extends React.Component {
-  render() {
-    const {currentTrackIndex, tracks} = this.props;
-    if(!tracks.length) {
-      return <div/>;
-    }
-    const track = tracks[currentTrackIndex];
-    const index = currentTrackIndex + 1;
-
-    return (
-      <div className="current-track navbar-left">
-        <span onClick={this.props.scrollToCurrentTrack} className="navbar-text current-track__title">
-          {index}. <span dangerouslySetInnerHTML={{__html: track.artist}}></span> - <span dangerouslySetInnerHTML={{__html: track.title}}></span>
-        </span>
-      </div>
-    );
+export const ActiveTrack = ({
+  currentTrackIndex,
+  tracks,
+  scrollToCurrentTrack
+}) => {
+  if(!tracks.length) {
+    return <div/>;
   }
-}
+  const track = tracks[currentTrackIndex];
+  const index = currentTrackIndex + 1;
+
+  return (
+    <div className="current-track navbar-left">
+      <span onClick={scrollToCurrentTrack} className="navbar-text current-track__title">
+        {index}. <span dangerouslySetInnerHTML={{__html: track.artist}}></span> - <span dangerouslySetInnerHTML={{__html: track.title}}></span>
+      </span>
+    </div>
+  );
+};
