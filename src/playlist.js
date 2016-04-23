110e7fdc608db51728f16727ef9a0c2f3dd318d9
jincod <vadim.abdrashitov@gmail.com>
2016-04-23 15:10:54 +0500

    hello screen

--- src/playlist.js
+++ src/playlist.js
@@ -8,9 +8,9 @@ export class PlayList extends React.Component {
   }
   renderEmpty() {
     return (
-      <div>
-        <h4>Take wall id or user id in search form or choose examples</h4>
-        <Link to="/wall-20833574">BBC Radio 1 / 1Xtra</Link>
+      <div className="hello-block">
+        <h4 className="hello-block__title">Take wall id or user id in search form or choose examples</h4>
+        <Link to="/wall-20833574" className="hello-block__link">BBC Radio 1 / 1Xtra</Link>
       </div>
     );
   }
