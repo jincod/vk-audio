df96d7b3977c60dd59bbd1d1bcc1d1d1dbb9dd10
jincod <vadim.abdrashitov@gmail.com>
2016-06-18 22:36:25 +0500

    added build script

---
+++ scripts/build.js
@@ -0,0 +1,21 @@
+var webpack = require('webpack');
+var config = require('../webpack/webpack.config.prod');
+
+if (process.env.NODE_ENV !== 'production') {
+	process.exit(0);
+}
+
+webpack(config, function(err, stats) {
+	var exitCode = 0;
+    if (err) {
+    	console.log(err);
+    	exitCode = 1;
+    }
+
+    if (stats.hasErrors() || stats.hasWarnings()) {
+    	exitCode = 1;
+    }
+
+  	console.log(stats.toString());
+  	process.exit(exitCode);
+});
